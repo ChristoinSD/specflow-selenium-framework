a321707f6c0a01df35d274d990d89d7c2a06ae82
markwinspear <markwinspear@googlemail.com>
2015-11-19 10:52:37 +0000

    updated bat files

--- Specflow_Selenium_PO_Example2/Run Specflow enhanced reporting (Step 2) .bat
+++ Specflow_Selenium_PO_Example2/Run Specflow enhanced reporting (Step 2) .bat
@@ -3,5 +3,5 @@ specflow nunitexecutionreport^
  Specflow_Selenium_PO_Example2.csproj^
  /out:"TestResult.html"^
  /xsltFile:"E:\Google Drive\Documents\Cucumber_Selenium_CSharp\specflow-report-templates\nunit-dream\ExecutionReport.xslt"^
- /xmlTestResult:"E:\Google Drive\Documents\Cucumber_Selenium_CSharp\Specflow_Selenium_PO_Example2\packages\NUnit.Runners.2.6.4\tools\TestResult.xml"
+ /xmlTestResult:"E:\Google Drive\Documents\Cucumber_Selenium_CSharp\specflow-selenium-framework\packages\NUnit.Runners.2.6.4\tools\TestResult.xml"
 pause
--- Specflow_Selenium_PO_Example2/run pickles.bat
+++ Specflow_Selenium_PO_Example2/run pickles.bat
@@ -1,6 +1,6 @@
-cd /D E:\"Google Drive"\Documents\Cucumber_Selenium_CSharp\Specflow_Selenium_PO_Example2
+cd /D E:\"Google Drive"\Documents\Cucumber_Selenium_CSharp\specflow-selenium-framework
 .\packages\Pickles.CommandLine.2.0.0\tools\pickles.exe^
- --feature-directory=./Specflow_Selenium_PO_Example2\Features
+ --feature-directory=./Specflow_Selenium_PO_Example2\Features^
  --output-directory=.\documentation^
  --test-results-format=specrun^
  --link-results-file=.\bin\Debug\TestResult.xml
