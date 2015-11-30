7bfe85ecd05e7eddc730bfc4a955e10cf02a6ebe
markwinspear <markwinspear@googlemail.com>
2015-11-30 14:55:19 +0000

    partly addreses issue #1

--- Specflow_Selenium_PO_Example2/Utils/Hooks.cs
+++ Specflow_Selenium_PO_Example2/Utils/Hooks.cs
@@ -125,6 +125,40 @@ namespace Specflow_Selenium_PO_Example2.Utils
                 Console.WriteLine("Error while taking screenshot: {0}", ex);
             }
         }
+
+        private static void TakeScreenshot(CustomRemoteDriver driver)
+        {
+            try
+            {
+                string fileNameBase = string.Format("error_{0}_{1}_{2}",
+                    FeatureContext.Current.FeatureInfo.Title,
+                    ScenarioContext.Current.ScenarioInfo.Title,
+                    DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+
+                var artifactDirectory = Path.Combine(Directory.GetCurrentDirectory(), "testresults");
+                if (!Directory.Exists(artifactDirectory))
+                    Directory.CreateDirectory(artifactDirectory);
+
+                string pageSource = driver.PageSource;
+                string sourceFilePath = Path.Combine(artifactDirectory, fileNameBase + "_source.html");
+                File.WriteAllText(sourceFilePath, pageSource, Encoding.UTF8);
+                Console.WriteLine("Page source: {0}", new Uri(sourceFilePath));
+
+                ITakesScreenshot takesScreenshot = driver as ITakesScreenshot;
+
+                if (takesScreenshot != null)
+                {
+                    var screenshot = takesScreenshot.GetScreenshot();
+                    string screenshotFilePath = Path.Combine(artifactDirectory, fileNameBase + "_screenshot.png");
+                    screenshot.SaveAsFile(screenshotFilePath, ImageFormat.Png);
+                    Console.WriteLine("Screenshot: {0}", new Uri(screenshotFilePath));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while taking screenshot: {0}", ex);
+            }
+        }
     }
 }
             
